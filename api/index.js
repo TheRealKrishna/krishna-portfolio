4c0496a9a4cdb65869f9e0772eba1b2e9da8807b
therealkrishna <agarwl.krishna2@gmail.com>
2025-01-23 12:28:07 +0530

    remove unused GET route for root in API index

--- api/index.js
+++ api/index.js
@@ -9,10 +9,6 @@ const axios = require('axios');
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('backend for Krishna Portfolio')
-})
-
 app.post("/contact", async (req, res) => {
     try {
         const captchaResponse = await axios.get(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${req.body.reCaptchaToken}`)
