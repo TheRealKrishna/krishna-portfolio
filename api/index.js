3b87d52013032390273b63c1f2ca52baea303a96
therealkrishna <agarwl.krishna2@gmail.com>
2025-01-23 18:21:21 +0530

    fix: update captcha response handling to access data property

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 app.post("/contact", async (req, res) => {
     try {
         const captchaResponse = await axios.get(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${req.body.reCaptchaToken}`)
-        if (!captchaResponse.success) {
+        if (!captchaResponse.data.success) {
             return res.status(409).json({ success: false, error: "Invalid Captcha Response!" })
         }
         const transport = nodemailer.createTransport({
