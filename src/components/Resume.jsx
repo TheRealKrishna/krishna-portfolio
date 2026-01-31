fbf1660220e7573392274ff42946bca278ca36be
therealkrishna <agarwl.krishna2@gmail.com>
2026-01-31 16:16:22 +0530

    resume update

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,7 +6,7 @@ export default function Resume() {
 
   function handleResume() {
     if (window.location.pathname === "/resume") {
-      window.open("https://drive.google.com/file/d/1JJG1Qtet02jifRM2rtrqczS_jVIFhlcz/view?usp=sharing", "_self")
+      window.open("https://drive.google.com/file/d/15CUK0FjRl1HLqExISN_VZZR_QZbFYJ3Y/view?usp=sharing", "_self")
     }
   }
 
