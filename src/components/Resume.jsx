98a2f830ddd577b6b50f94d9e93b52b2287481b3
therealkrishna <agarwl.krishna2@gmail.com>
2026-02-04 22:30:25 +0530

    fix: update handleResume function to set body overflow and return loading overlay for resume path

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,10 +1,10 @@
 import React, { useLayoutEffect } from "react";
 
 export default function Resume() {
-
   function handleResume() {
     if (window.location.pathname === "/resume") {
-      window.open("https://drive.google.com/file/d/15CUK0FjRl1HLqExISN_VZZR_QZbFYJ3Y/view?usp=sharing", "_self")
+      document.body.style.overflow = "hidden";
+      window.open("https://drive.google.com/file/d/15CUK0FjRl1HLqExISN_VZZR_QZbFYJ3Y/view?usp=sharing", "_self", );
     }
   }
 
@@ -12,5 +12,17 @@ export default function Resume() {
     handleResume();
   }, []);
 
-  return <></>;
+  if (window.location.pathname === "/resume") {
+    return (
+      <div style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        background: "black",
+        zIndex: 9999
+      }}></div>
+    );
+  }
 }
